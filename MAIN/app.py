e6e89a7574ac40ba3ec84f5f3f527880a209e977
ethanoschong <ethanchong273@gmail.com>
2025-01-21 09:30:47 +0800

    added admin template

--- MAIN/app.py
+++ MAIN/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/admin/')
+def admin():
+    return render_template('admin_panel.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
