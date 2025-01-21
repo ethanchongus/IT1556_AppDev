4342c750e08dfe12969414155c4a5d7d1e9bb246
Ayden000 <yhlee.ayden@gmail.com>
2025-01-21 10:39:12 +0800

    added admin transaction

--- MAIN/app.py
+++ MAIN/app.py
@@ -9,5 +9,11 @@ def index():
 def admin():
     return render_template('admin_panel.html')
 
+@app.route('/admin/transaction/')
+def admin_transaction():
+    return render_template('admin_transaction.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
