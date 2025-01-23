a11ae990e830f5f4ec7da3e2ef54679a09332a67
Ayden000 <yhlee.ayden@gmail.com>
2025-01-23 14:47:51 +0800

    Restart

--- app.py
+++ app.py
@@ -1,42 +1,18 @@
 from flask import Flask, render_template
-from transaction import transaction_bp  # Import the Blueprint
-from cart import cart_bp  # Import the cart Blueprint
 
+# Initialize the Flask app
 app = Flask(__name__)
 
 # Set the secret key for the Flask app
-app.secret_key = "eatmylampa"  # Replace with a strong, unique key
-
-# Register Blueprints
-app.register_blueprint(cart_bp, url_prefix='/cart')
-app.register_blueprint(transaction_bp, url_prefix='/transaction')
+app.secret_key = "secret"  # Replace with a strong, unique key
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/transaction/')
-def transaction():
-    return render_template('customer_transaction.html')
-
-
 @app.route('/admin/')
 def admin():
     return render_template('admin_panel.html')
-
-@app.route('/admin/transaction/')
-def admin_transaction():
-    return render_template('admin_transaction.html')
-
-@app.route('/activities/')
-def activities():
-    return render_template('customer_activities.html')
-
-# Route for cart page
-@app.route('/cart/')
-def cart():
-    cart = session.get('cart', [])
-    return render_template('customer_cart.html', cart=cart)
 
 if __name__ == '__main__':
     app.run(debug=True)
