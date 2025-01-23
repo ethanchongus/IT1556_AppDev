eaea5b357bf49f3e2c50829840fd4cf91d602fe0
Ayden000 <yhlee.ayden@gmail.com>
2025-01-23 18:15:47 +0800

    Payment done

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from payment_routes import payment_bp
+from admin_routes import admin_bp
 
 # Initialize the Flask app
 app = Flask(__name__)
-
 # Set the secret key for the Flask app
 app.secret_key = "secret"  # Replace with a strong, unique key
+
+# Register Blueprints
+app.register_blueprint(payment_bp, url_prefix='/payment')
+app.register_blueprint(admin_bp, url_prefix='/admin/payments')
 
 @app.route('/')
 def index():
