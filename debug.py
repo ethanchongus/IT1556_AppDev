cb4972279bb42cf3561c5d586467959bc5c3e037
ethanoschong <ethanchong273@gmail.com>
2025-02-14 23:37:54 +0800

    removed debug

--- debug.py
+++
@@ -1,7 +0,0 @@
-import shelve
-purchase_db = 'database/purchase_data'
-
-with shelve.open(purchase_db, 'r') as db:
-    for key in db:
-        purchase = db[key]
-        print(f"Purchase ID: {purchase.get_purchase_id()}, User Email: {purchase.get_user_email()}, Tour: {purchase.get_tour_name()}, Seats: {purchase.get_seats()}")
